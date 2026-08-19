51c47c790167e96dcfa4264d2d86b83961285e9e
MS Tech Hive <mstechhive@gmail.com>
2026-08-19 16:47:27 +0530

    Ensure cart is strictly cleared when switching to an empty space/table

--- Frontend/src/components/BillingPage.jsx
+++ Frontend/src/components/BillingPage.jsx
@@ -609,8 +609,8 @@ const BillingPage = ({ initialTable, onOrderUpdate, onNavigate, onGoBack, userRo
         const msSinceEdit = Date.now() - lastLocalEditTime.current;
         const isEditLocked = msSinceEdit < LOCAL_EDIT_LOCK_MS;
         
-        if (isEditLocked) {
-          // If the user just edited the cart, the backend might not have saved it yet.
+        if (isEditLocked && !forceReset) {
+          // If the user just edited the cart for THIS table, the backend might not have saved it yet.
           // DO NOT clear the cart, otherwise the UI will blink empty!
           return;
         }
